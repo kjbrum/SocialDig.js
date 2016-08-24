103b4a78cc0a3d8723083180f3b99c38be82fe6b
Kyle Brumm <kjbrumm@gmail.com>
2016-08-24 12:51:47 -0500

    Start building the project website

--- js/src/main.js
+++ js/src/main.js
@@ -1,4 +1,29 @@
 ;
 (function(global) {
-    // Code goes here
+    /**
+     *  Social sharing popup window
+     */
+    var jsSocialShares = document.querySelectorAll('.social-share a');
+    if(jsSocialShares) {
+        [].forEach.call(jsSocialShares, function(anchor) {
+            anchor.addEventListener('click', function(e) {
+
+                var url = this.href,
+                    width = 500,
+                    height = 300,
+                    left = (screen.width / 2) - (width / 2),
+                    top = (screen.height / 2) - (height / 2);
+
+                if(/^(f|ht)tps?:\/\//i.test(url) || /^mailto/i.test(url)) {
+                    e.preventDefault();
+                    window.open(
+                        url,
+                        '',
+                        'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,width=' + width + ',height=' + height + ',top=' + top + ',left=' + left
+                    );
+                }
+
+            });
+        });
+    }
 })(window);
